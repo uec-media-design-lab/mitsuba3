28f68af4b40d5fdcc4676140907e051def613a5a
Ashroom66 <ashroom09@gmail.com>
2023-07-19 21:49:29 +0900

    feat: RRR constructor

--- src/bsdfs/roughretroreflector.cpp
+++ src/bsdfs/roughretroreflector.cpp
@@ -34,9 +34,104 @@ public:
     MI_IMPORT_TYPES(Texture)
 
     RoughRetroreflector(const Properties &props) : Base(props) {
-        m_components.push_back(BSDFFlags::Null | BSDFFlags::FrontSide | BSDFFlags::BackSide);
-        m_flags = m_components.back();
-        dr::set_attr(this, "flags", m_flags);
+        m_pa = Normal3f(2*math::InvSqrtSix<Float>, 0, math::InvSqrtThree<Float>);                         // →
+        m_qa = Normal3f(-math::InvSqrtSix<Float>, math::InvSqrtTwo<Float>, math::InvSqrtThree<Float>);    // ↖
+        m_ra = Normal3f(-math::InvSqrtSix<Float>, -math::InvSqrtTwo<Float>, math::InvSqrtThree<Float>);   // ↙
+        m_pb = Normal3f(-2*math::InvSqrtSix<Float>, 0, math::InvSqrtThree<Float>);                       // ←
+        m_qb = Normal3f(math::InvSqrtSix<Float>, math::InvSqrtTwo<Float>, math::InvSqrtThree<Float>);    // ↗
+        m_rb = Normal3f(math::InvSqrtSix<Float>, -math::InvSqrtTwo<Float>, math::InvSqrtThree<Float>);   // ↘
+
+        // # iors
+        ScalarFloat int_ior = lookup_ior(props, "int_ior", "bk7");
+        ScalarFloat ext_ior = lookup_ior(props, "ext_ior", "air");
+        std::string material = props.string("base_material", "none");
+        if (props.has_property("base_eta") || material == "none") {
+            // etaで指定されてる場合
+            // デフォでは屈折率実部0, 虚部1で完全に吸収する素材が指定される
+            m_eta_base = props.texture<Texture>("base_eta", 0.f);
+            m_k_base   = props.texture<Texture>("base_k",   1.f);
+            if (material != "none")
+                Throw("Should specify either (eta, k) or material, not both.");
+        } else {
+            // 材質が指定されてる場合は参照する。見つからなければCu
+            std::tie(m_eta_base, m_k_base) = complex_ior_from_file<Spectrum, Texture>(props.string("base_material", "Al"));
+        }
+
+        if (int_ior < 0.f || ext_ior < 0.f || int_ior == ext_ior)
+            Throw("The interior and exterior indices of "
+                  "refraction must be positive and differ!");
+        m_eta = int_ior / ext_ior;
+        m_inv_eta = ext_ior / int_ior;
+        m_eta_mat = int_ior;
+        m_eta_air = ext_ior;
+
+        // # distributions
+        if (props.has_property("distribution")) {
+            std::string distr = string::to_lower(props.string("distribution"));
+            if (distr == "beckmann")
+                m_type = MicrofacetType::Beckmann;
+            else if (distr == "ggx")
+                m_type = MicrofacetType::GGX;
+            else
+                Throw("Specified an invalid distribution \"%s\", must be "
+                      "\"beckmann\" or \"ggx\"!", distr.c_str());
+        } else {
+            m_type = MicrofacetType::Beckmann;
+        }
+
+        if (props.has_property("alpha_u_surface") || props.has_property("alpha_v_surface")) {
+            if (!props.has_property("alpha_u_surface") || !props.has_property("alpha_v_surface"))
+                Throw("Microfacet model: both 'alpha_u' and 'alpha_v' must be specified.");
+            if (props.has_property("alpha_surface"))
+                Throw("Microfacet model: please specify"
+                      "either 'alpha' or 'alpha_u_surface'/'alpha_v_surface'.");
+            m_alpha_u_surface = props.texture<Texture>("alpha_u_surface");
+            m_alpha_v_surface = props.texture<Texture>("alpha_v_surface");
+        } else if (props.has_property("alpha_surface")) {
+            m_alpha_u_surface = m_alpha_v_surface = props.texture<Texture>("alpha_surface", 0.1f);
+        }
+        if (props.has_property("alpha_u_internal") || props.has_property("alpha_v_internal")) {
+            if (!props.has_property("alpha_u_internal") || !props.has_property("alpha_v_internal"))
+                Throw("Microfacet model: both 'alpha_u' and 'alpha_v' must be specified.");
+            if (props.has_property("alpha_internal"))
+                Throw("Microfacet model: please specify"
+                      "either 'alpha' or 'alpha_u_internal'/'alpha_v_internal'.");
+            m_alpha_u_internal = props.texture<Texture>("alpha_u_internal");
+            m_alpha_v_internal = props.texture<Texture>("alpha_v_internal");
+        } else if (props.has_property("alpha_internal")) {
+            m_alpha_u_internal = m_alpha_v_internal = props.texture<Texture>("alpha_internal", 0.1f);
+        }
+        if (props.has_property("alpha")) {
+            m_alpha_u_surface = m_alpha_v_surface = m_alpha_u_internal = m_alpha_v_internal =props.texture<Texture>("alpha", 0.1f);
+        }
+
+        // # flags
+        BSDFFlags anistropic = ((m_alpha_u_surface != m_alpha_v_surface) || (m_alpha_u_internal != m_alpha_v_internal)) ? BSDFFlags::Anisotropic : BSDFFlags(0);
+        // # reflection, retroreflection, diffuse
+        m_components.push_back(BSDFFlags::GlossyReflection | BSDFFlags::FrontSide | BSDFFlags::BackSide | anistropic);
+        m_components.push_back(BSDFFlags::GlossyReflection | BSDFFlags::GlossyTransmission | BSDFFlags::FrontSide | BSDFFlags::BackSide | anistropic);
+        m_components.push_back(BSDFFlags::DiffuseReflection | BSDFFlags::FrontSide);
+        m_flags= m_components[0] | m_components[1] | m_components[2];
+    
+        // sample_visible
+        m_sample_visible = props.bool_("sample_visible", true);
+
+        // diffuseFactor
+        Float F_d = 0.f;
+        if (m_eta<1.f) {
+            F_d = 0.919317 - 3.4793*m_eta + 6.75335*m_eta*m_eta - 7.80989*m_eta*m_eta*m_eta + 4.98554*m_eta*m_eta*m_eta*m_eta - 1.36881*m_eta*m_eta*m_eta*m_eta*m_eta;
+        } else {
+            F_d = -9.23372 + 22.2272*m_eta - 20.9292*m_eta*m_eta + 10.2291*m_eta*m_eta*m_eta - 2.54396*m_eta*m_eta*m_eta*m_eta + 0.254913*m_eta*m_eta*m_eta*m_eta*m_eta;
+        }
+        diffuseFactor = math::InvPi<Float> * sqr(m_inv_eta) / (1.f - F_d);
+        
+        // surface normal(ideal)
+        n = Normal3f(0.f, 0.f, 1.f);
+
+        // reflectance
+        m_surface_reflectance   = props.float_("surface_reflectance", 1.f);
+    
+        m_internal_reflectance   = props.float_("internal_reflectance", 1.f);
     }
 
     void traverse(TraversalCallback *callback) override {
